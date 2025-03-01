d2b43cec0b2fb44383f237b7f4009460dd0af9c2
Vaibhav Kumar <kumarvaibhav077@gmail.com>
2025-03-01 21:39:26 +0530

    adding logs and data

--- ingestion.py
+++ ingestion.py
@@ -1,6 +1,26 @@
-import kagglehub
- 
+import pandas as pd
+import logging
+import os
+
+# Ensure logs directory exists
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
 # Download latest version
-path = kagglehub.dataset_download("muhammadshahidazeem/customer-churn-dataset")
- 
-print("Path to dataset files:", path)
+log_file = os.path.join(log_dir, "app.log")
+logging.basicConfig(
+    filename=log_file,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+try:
+    path= "data\customer_churn_dataset-training-master.csv"
+    data = pd.read_csv(path)
+    logging.info("Data ingested")
+except:
+    # Configure logging
+
+# Example log messages
+    logging.error("Data ingestion failed")
+
+print(data.head(5))
+
